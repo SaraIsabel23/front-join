c4b5d43281e48ef84e6137c3541a782e38e25c80
Sara Isabel del Sastre <chinchimilla@gmail.com>
2026-03-22 17:04:36 +0100

    front terminado

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,20 @@
-const Home = () => {
+import {Link} from 'react-router-dom'
+
+const Home = ({data}) => {
   return (
     <>
-    
+    <h2>Lista de datos</h2>
+    <ul>
+      {data.map(item => (
+        <li key={item._id}>
+          <Link to={`/${item._id}`}>{item.title}</Link>
+        </li>
+      ))}
+    </ul>
     </>
+
   )
+  
 };
 
 export default Home;
